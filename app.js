88961c985e56a3ebf1d7fe6e3889804f199b67ec
snehomoy100 <snehomoy100@gmail.com>
2021-04-19 01:13:49 +0530

    app.js file updated

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', "I am Snehomoy");
+// fs.writeFileSync('notes.txt', "I am Snehomoy");
+
+fs.appendFileSync('notes.txt', ', I live in India...!');
 
